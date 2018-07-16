2a6453d20660748751d1f91455c23eda6f8e3079
josnunezg <josnunezg@gmail.com>
2018-07-16 17:27:05 -0400

    create categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Category.create(name: "food")
+Category.create(name: "home")
+Category.create(name: "purchases")
